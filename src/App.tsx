90a8ec255cf972562364531b8979474ec756a7f4
Julian Grey <julianrgrey@gmail.com>
2025-10-07 19:44:54 +0100

    Create functionality to pull json data
    Using sample data from the LMU API of a practice server, setup functionality for pulling data in an expected format.

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import { useState } from 'react';
 import './App.scss';
+import sessionStatus from './assets/session-status.json';
+
+interface SessionStatus {
+  bestLapTime: number,
+  carClass: string,
+  driverName: string,
+  lapsBehindLeader: number,
+  lapsBehindNext: number,
+  lapsCompleted: number,
+  pitting: boolean,
+  timeBehindLeader: number,
+  timeBehindNext: number,
+}
 
 function App() {
+  const [raceData, setRaceData] = useState<SessionStatus[]>();
+
+  function updateData() {
+    setRaceData(sessionStatus as SessionStatus[]);
+  }
+
   return (
-    <></>
+    <div className='lmu-overlay'>
+      <div>
+        <ul>
+          {
+            raceData && raceData.map((driver, index) => (
+              <li key={index}>
+                {driver.driverName}
+              </li>
+            ))
+          }
+        </ul>
+      </div>
+      <button onClick={updateData}>Display data</button>
+    </div>
   );
 }
 
