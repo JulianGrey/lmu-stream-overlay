acf412d0df6e2554f5fc0f55ddc301aa783f3b29
Julian Grey <julianrgrey@gmail.com>
2025-10-08 12:02:07 +0100

    Add styling and adjustments to standings

--- src/components/Standings/Standings.tsx
+++ src/components/Standings/Standings.tsx
@@ -1,28 +1,26 @@
-
+import styles from './Standings.module.scss';
 import type { StandingsProps } from '../../types';
 
 export default function Standings({raceData, updateData}: StandingsProps) {
   return (
     <>
-      <div>
-        <ul>
-          {
-            raceData && raceData.map((driver, index) => (
-              <li key={index} className='row'>
-                <div className='position'></div>
-                <div className='car-class'>{driver.carClass}</div>
-                <div className='driver-name'>{driver.driverName}</div>
-                {(driver.lapsBehindLeader < 1) && (
-                  <div className='time-behind-leader'>-{driver.timeBehindLeader} laps</div>
-                )}
-                {(driver.lapsBehindLeader > 0) && (
-                  <div className='time-behind-leader'>-{driver.lapsBehindLeader}</div>
-                )}
-              </li>
-            ))
-          }
-        </ul>
-      </div>
+      <ul className={styles.standings}>
+        {
+          raceData && raceData.map((driver, index) => (
+            <li key={(index + 1)} className={styles.row}>
+              <div className={styles.position}>{(index + 1)}</div>
+              <div className={styles['car-class']}>{driver.carClass}</div>
+              <div className={styles['driver-name']}>{driver.driverName}</div>
+              {(driver.lapsBehindLeader < 1) && (
+                <div className={styles['time-behind-leader']}>-{driver.timeBehindLeader.toFixed(2)}</div>
+              )}
+              {(driver.lapsBehindLeader > 0) && (
+                <div className={styles['time-behind-leader']}>-{driver.lapsBehindLeader.toFixed(0)} laps</div>
+              )}
+            </li>
+          ))
+        }
+      </ul>
       <button onClick={updateData}>Display data</button>
     </>
   );
